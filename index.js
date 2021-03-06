3f6f035415b38215cc35071e917aaa06f70e4c94
jwatts82 <jessperr182@hotmail.com>
2021-03-05 21:15:22 -0800

    commit tree df1f32fafdc6cff8293bdbfc1e0646bbc3f33662

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 function findMinAndRemove(array){
-
+    let min = array[0] 
+    let minIndex = 0 
+    array.forEach( arr =>  { 
+        if (arr < min) { 
+            min = arr 
+            minIndex = array.indexOf(arr)
+        }
+    })
+    array.splice(minIndex, 1)
+    return min
+    // for(let i = 0; i < array.length; i++) { 
+//         if(array[i] < min) { 
+//             min = array[i] 
+//             minIndex = i 
+//         }
+//     }
+//     array.splice(minIndex, 1)
+//     return min
 }
 
-function selectionSort(array){
-
+function insertionSort(array){
+    let newMin
+    let sorted = []
+    while (array.length !== 0) { 
+        let newMin = findMinAndRemove(array)
+        sorted.push(newMin)
+    }
+    return sorted
 }
